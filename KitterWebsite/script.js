1e4a29e70f4e9da815a9a274edee63991642c759
Sena <151879995+senaxx3@users.noreply.github.com>
2024-05-12 12:53:08 +0300

    final
    Final

--- KitterWebsite/script.js
+++ KitterWebsite/script.js
@@ -34,16 +34,20 @@ const closeNavbar = function () {
 addEventOnElem(navbarLinks, "click", closeNavbar);
 
 
-// active header //
+// active header scroll down 100px //
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const activeElemOnScroll = function () {
     if (window.scrollY > 100){
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     }else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 }
 
 addEventOnElem(window , "scroll" , activeElemOnScroll);
+
